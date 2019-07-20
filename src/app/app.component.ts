e5a54632fac8ddc0d6e38faae2d921292a158974
jeet-s <jagjeet@simplifiedautomation.ca>
2019-07-20 17:55:13 +0530

    components styles and demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { SaButtonConfig, SaButtonType, IDataFilterViewModel, FilterTypeEnum, SaSelectConfig, DatePickerConfig, IHeaderViewModel, SaButton, SaMoreMenuItem, NavigationItem } from 'projects/simplified-ui/src/public-api';
+import { FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'simplified-ui-library';
+export class AppComponent implements OnInit {
+
+  @ViewChild("selectOptionBody") selectOptionBody;
+
+  isLoading: boolean = true;
+
+  saveButtonConfig = new SaButtonConfig('Save');
+
+  dataFilterConfig: IDataFilterViewModel;
+
+  dataFilterConfigArray: IDataFilterViewModel[] = [];
+
+  selectOptions= new SaSelectConfig<string>();
+
+  dateConfig = new DatePickerConfig();
+
+  headerConfig: IHeaderViewModel = {
+    title: "Header",
+    primaryButton: new SaButton("Primary"),
+    secondaryButton: new SaButton("Secondary"),
+    moreMenu: null,
+  }
+
+  menuItems: SaMoreMenuItem[] = [
+    new SaMoreMenuItem("Item 1"),
+    new SaMoreMenuItem("Item 2")
+  ];
+
+  primaryNavMenu: NavigationItem[] = [
+    new NavigationItem("Nav 1", null),
+    new NavigationItem("Nav 2", null),
+  ];
+
+  secondaryNavMenu: NavigationItem[] = [
+    new NavigationItem("Nav 1", null),
+    new NavigationItem("Nav 2", null),
+  ];
+
+  editorForm = this.form.group({
+    description: new FormControl(),
+  });
+
+  constructor(private form: FormBuilder){}
+
+  ngOnInit(){
+
+    this.selectOptions.templateRef = this.selectOptionBody;
+    this.selectOptions.options.next(["Option 1"]);
+
+
+    this.dataFilterConfig =  {
+  key: 'id',
+  filterType: FilterTypeEnum.select,
+  config: this.selectOptions,
+  title: 'filter'
+};
+
+this.dataFilterConfigArray.push(this.dataFilterConfig);
+
+    this.saveButtonConfig.isSpinning = false;
+    this.saveButtonConfig.loadingText = 'Saving';
+    this.saveButtonConfig.type = SaButtonType.Anchor;
+
+    this.saveButtonConfig.clickSubject.subscribe(
+      () => {
+        this.saveButtonConfig.isSpinning = !this.saveButtonConfig.isSpinning;
+        console.log(this.saveButtonConfig.isSpinning);
+      }
+    );
+
+  }
+
+  
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SimplifiedUiModule } from '../../projects/simplified-ui/src/lib/simplified-ui.module'
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatIconModule } from '@angular/material';
+import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,9 +15,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SimplifiedUiModule,
+    BrowserAnimationsModule,
+    OwlDateTimeModule,
+    OwlNativeDateTimeModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [,
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } },],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

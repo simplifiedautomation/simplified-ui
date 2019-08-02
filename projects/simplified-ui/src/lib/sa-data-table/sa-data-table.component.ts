6d5570e43643e5a4bfb968877ef720ce1e852d83
Ishmeet Sethi <sethi.ishmeet@gmail.com>
2019-08-03 00:25:27 +0530

    added observers for column and row add

--- projects/simplified-ui/src/lib/sa-data-table/sa-data-table.component.ts
+++ projects/simplified-ui/src/lib/sa-data-table/sa-data-table.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input, ViewChild, Output, EventEmitter, AfterViewInit, SimpleChanges, OnChanges } from '@angular/core';
 import { MatSort, MatPaginator } from '@angular/material';
 import { SelectionModel } from '@angular/cdk/collections';
-import { IDataTable, IRequestModel } from '../models/DataTableModel';
+import { DataTable, IRequestModel } from '../models/DataTableModel';
 import { SaTableDataSource } from '../services/sa-table-data-source.service';
 import { BehaviorSubject, Subscription, Observable } from 'rxjs';
 import { DefaultCommonTableFilter, SaCommonTableFilter } from '../models/SaTableDataSource';
@@ -17,10 +17,12 @@ import { IDataFilterViewModel, IFilterModel } from '../models/DataFilterModels';
   templateUrl: './sa-data-table.component.html',
   styleUrls: ['./sa-data-table.component.scss']
 })
-export class SaDataTableComponent<T> implements OnInit, AfterViewInit, OnChanges {
+export class SaDataTableComponent<T> implements OnInit, AfterViewInit {
 
-  @Input() dataTable: IDataTable<T>;
+  @Input() dataTable: DataTable<T>;
+
   @Output() rowClick = new EventEmitter<T>();
+  @Output() rowSelect = new EventEmitter<T[]>();
 
   public columnToDisplay: string[] = [];
   @ViewChild('iconSelector') iconSelector;
@@ -81,11 +83,15 @@ export class SaDataTableComponent<T> implements OnInit, AfterViewInit, OnChanges
 
   ngOnInit() {
 
-    this._setColumns();
+    this.dataTable.onColumnUpdated().subscribe(columns => {
+      columns.forEach(z => {
+        if (z.filter != null)
+          this.filterArray.push(z.filter);
+      });
 
-    this.dataTable.columns.forEach(z => {
-      if (z.filter != null)
-        this.filterArray.push(z.filter);
+      this.columnToDisplay = columns.map(z => {
+        return z.key;
+      });
     });
 
     this.tableDataSource = new SaTableDataSource(
@@ -119,15 +125,9 @@ export class SaDataTableComponent<T> implements OnInit, AfterViewInit, OnChanges
       this.columnToDisplay.push('options');
     }
 
-    this.dataTable.dataSource.subscribe(x => {
+    this.dataTable.onRowAdded().subscribe(x => {
       this._source.next([...[x], ...this._source.value]);
     })
-  }
-
-  ngOnChanges(changes: SimpleChanges){
-    if (changes.dataTable){
-      this._setColumns();
-    }
   }
 
   ngAfterViewInit(): void {
@@ -150,7 +150,7 @@ export class SaDataTableComponent<T> implements OnInit, AfterViewInit, OnChanges
 
   //* Callback for when table filter change is triggered.
   //  * Returns a new observable to fetch the new list of records using the updated filter model
-  //* 
+  //*
   //  * @param filter updated table filter model
   //* /
   private _getRecords(filter: SaCommonTableFilter): Observable<IGenericPageListViewModel<T>> {
@@ -166,19 +166,13 @@ export class SaDataTableComponent<T> implements OnInit, AfterViewInit, OnChanges
     return this.dataTable.getResults(requestModel);
   }
 
-  private _setColumns(){
-    this.columnToDisplay = this.dataTable.columns.map(z => {
-      return z.key;
-    });
-  }
-
   filterChange(filter: IFilterModel) {
     this.tableDataSource.filter.pageNo = 0;
     this.tableDataSource.filter.filterModel = filter;
   }
 
   dataRowClick(row: T) {
-    this.dataTable.rowClick(row);
+    this.rowClick.emit(row);
   }
 
   openSubMenuOptions() {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { SaButtonConfig, SaButtonType, IDataFilterViewModel, FilterTypeEnum, SaSelectConfig, DatePickerConfig, IHeaderViewModel, SaMoreMenuItem, NavigationItem, IDataTable, IDataTableColumn, DataTableColumnTypeEnum, IRequestModel, IGenericPageListViewModel } from 'projects/simplified-ui/src/public-api';
+import { SaButtonConfig, SaButtonType, IDataFilterViewModel, FilterTypeEnum, SaSelectConfig, DatePickerConfig, IHeaderViewModel, SaMoreMenuItem, NavigationItem, DataTable, IDataTableColumn, DataTableColumnTypeEnum, IRequestModel, IGenericPageListViewModel } from 'projects/simplified-ui/src/public-api';
 import { FormControl, FormBuilder } from '@angular/forms';
 import { Subject, Observable, of } from 'rxjs';
 
@@ -11,9 +11,9 @@ import { Subject, Observable, of } from 'rxjs';
 export class AppComponent implements OnInit {
 
   @ViewChild("selectOptionBody") selectOptionBody;
-  
-  items = Array.from({length: 100000}).map((_, i) => `Item #${i}`);
-  
+
+  items = Array.from({ length: 100000 }).map((_, i) => `Item #${i}`);
+
   isLoading: boolean = true;
 
   isSpin: boolean = true;
@@ -42,7 +42,7 @@ export class AppComponent implements OnInit {
     currency: new FormControl()
   }
   );
-  
+
 
   menuItems: SaMoreMenuItem[] = [
     new SaMoreMenuItem("Item 1"),
@@ -80,7 +80,7 @@ export class AppComponent implements OnInit {
     new NavigationItem('5W1H', '5w1h'),
     new NavigationItem('4M1D', '4m1d'),
     new NavigationItem('5WHY', '5why'),
-    new NavigationItem('Results', 'results')    
+    new NavigationItem('Results', 'results')
   ];
 
   secondaryNavMenu: NavigationItem[] = [
@@ -94,7 +94,7 @@ export class AppComponent implements OnInit {
 
   kaizenType;
 
-  demoDataTable: DemoDataTable;
+  dataTable: DataTable<any> = new DataTable();
 
   constructor(private form: FormBuilder) { }
 
@@ -118,85 +118,24 @@ export class AppComponent implements OnInit {
     this.saveButtonConfig.type = SaButtonType.Anchor;
 
     this.currencyForm.controls.currency.setValue('100');
-
-    this.demoDataTable = new DemoDataTable();
-
+    this.setupDataTable();
   }
 
-  onClick(){
+  onClick() {
     this.primaryMenu = this.standardPrimaryMenu;
     this.kaizenType = 'standard';
   }
 
   onPrimaryClick(event) {
-    
+
   }
 
   onSecondaryClick(event) {
-    
+
   }
 
-}
-
-
-export class DemoDataTable implements IDataTable<any>
-{
-  dataSource: Subject<any> = new Subject();
-
-  columns: IDataTableColumn[] = [];
-
-  mainActionMenu: SaButtonConfig[] = [new SaButtonConfig('option 1')];
-
-  optionsMenu: SaButtonConfig[] = [];
-
-  isClientSide: boolean = true;
-
-  showCheckboxColumn: boolean = false;
-
-  showFilters: boolean = false;
-
-  constructor() {
-    this.setColumns();
-  }
-
-  private setColumns() {
-    this.columns.push({
-      key: "column1",
-      title: "COLUMN 1",
-      type: DataTableColumnTypeEnum.text,
-      filter: {
-        config: null,
-        filterType: FilterTypeEnum.none,
-        key: "column1"
-      },
-      stickyEnd: true
-    });
-
-    this.columns.push({
-      key: "column2",
-      title: "COLUMN 2",
-      type: DataTableColumnTypeEnum.text,
-      filter: {
-        config: null,
-        filterType: FilterTypeEnum.none,
-        key: "column2"
-      },
-      stickyEnd: true
-    });
-
-    this.columns.push({
-      key: "column3",
-      title: "COLUMN 3",
-      type: DataTableColumnTypeEnum.text,
-      filter: {
-        config: null,
-        filterType: FilterTypeEnum.none,
-        key: "column3"
-      },
-      stickyEnd: true
-    });
-
-    this.columns.push({
+  updateTable() {
+    this.dataTable.addColumn({
       key: "column4",
       title: "COLUMN 4",
       type: DataTableColumnTypeEnum.text,
@@ -208,6 +147,53 @@ export class DemoDataTable implements IDataTable<any>
       stickyEnd: true
     });
 
+    this.dataTable.addRow({
+      column1: 'column 1',
+      column2: 'column 2',
+      column3: 'column 3',
+      column4: 'column 4'
+    });
+  }
+
+  private setupDataTable() {
+    this.dataTable.addColumn({
+      key: "column1",
+      title: "COLUMN 1",
+      type: DataTableColumnTypeEnum.text,
+      filter: {
+        config: null,
+        filterType: FilterTypeEnum.none,
+        key: "column1"
+      },
+      stickyEnd: true
+    });
+
+    this.dataTable.addColumn({
+      key: "column2",
+      title: "COLUMN 2",
+      type: DataTableColumnTypeEnum.text,
+      filter: {
+        config: null,
+        filterType: FilterTypeEnum.none,
+        key: "column2"
+      },
+      stickyEnd: true
+    });
+
+    this.dataTable.addColumn({
+      key: "column3",
+      title: "COLUMN 3",
+      type: DataTableColumnTypeEnum.text,
+      filter: {
+        config: null,
+        filterType: FilterTypeEnum.none,
+        key: "column3"
+      },
+      stickyEnd: true
+    });
+
+    this.dataTable.getResults = this.getResults;
+    this.dataTable.isClientSide = true;
   }
 
   getResults(requestModel: IRequestModel): Observable<IGenericPageListViewModel<any>> {
@@ -234,6 +220,5 @@ export class DemoDataTable implements IDataTable<any>
       }
     );
   }
-
 
 }

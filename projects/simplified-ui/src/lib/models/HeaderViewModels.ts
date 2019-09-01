176ccad01496516727254c49c976a5ef239843ae
jeet-s <jagjeet@simplifiedautomation.ca>
2019-09-01 15:15:02 +0530

    added menutemplate in sa-header

--- projects/simplified-ui/src/lib/models/HeaderViewModels.ts
+++ projects/simplified-ui/src/lib/models/HeaderViewModels.ts
@@ -1,10 +1,12 @@
 import { SaButtonConfig } from '../sa-button/sa-button.component';
+import { TemplateRef } from '@angular/core';
 
 export interface IHeaderViewModel {
   title: string;
   primaryButton: SaButtonConfig;
   secondaryButton: SaButtonConfig;
   moreMenu: IMoreMenuViewModel;
+  menuTemplate?: TemplateRef<any>
 }
 
 export interface IMoreMenuViewModel {

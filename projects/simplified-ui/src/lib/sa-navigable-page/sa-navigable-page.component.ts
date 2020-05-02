0b16c43a21435f181995d0c144fdd1fe14370df3
anuraadha <anuradha@simplifiedautomation.ca>
2020-05-02 17:20:29 +0530

    fixed table of content responsiveness

--- projects/simplified-ui/src/lib/sa-navigable-page/sa-navigable-page.component.ts
+++ projects/simplified-ui/src/lib/sa-navigable-page/sa-navigable-page.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, NgZone } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NavigationItem } from '../models/NavigationItem';
 
+const SMALL_WIDTH_BREAKPOINT = 720;
 @Component({
   selector: 'sa-navigable-page',
   templateUrl: './sa-navigable-page.component.html',
@@ -12,10 +13,22 @@ export class SaNavigablePageComponent implements OnInit {
   @Input() primaryMenu: NavigationItem[] = [];
   @Input() secondaryMenu: NavigationItem[] = [];
 
-  constructor(private route: ActivatedRoute) {
+  navigationList: NavigationItem[];
+
+  private mediaMatcher: MediaQueryList =
+    matchMedia(`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`);
+
+  constructor(zone: NgZone, private route: ActivatedRoute) {
+    this.mediaMatcher.addListener(mql =>
+      zone.run(() => this.mediaMatcher = matchMedia(`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`)));
   }
 
+  isScreenSmall(): boolean {
+    return this.mediaMatcher.matches;
+  };
+
   ngOnInit() {
+    this.navigationList = this.primaryMenu.concat(this.secondaryMenu);
     this.route.fragment.subscribe(f => {
       const element = document.querySelector("a[name='" + f + "']")
       if (element) {

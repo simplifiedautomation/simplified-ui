ab2461605ab38a31e3dfe8768aa8572a17fca379
Manmohit Singh <manmohitsingh@Pahuls-MacBook-Pro.local>
2020-06-29 18:52:21 +0530

    Added code to remove extra text editor toolbar options

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,7 +35,20 @@ import { HttpClientModule } from '@angular/common/http';
       customOptions: [{
         import: 'formats/font',
         whitelist: ['mirza', 'roboto', 'aref', 'serif', 'sansserif', 'monospace']
-      }]
+      }],
+      modules: {
+          toolbar: [
+            ['bold', 'italic', 'underline', 'strike'], 
+            [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+            [{ 'script': 'sub'}, { 'script': 'super' }],      
+            [{ 'indent': '-1'}, { 'indent': '+1' }],       
+            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+            [{ 'color': [] }, { 'background': [] }],
+            [{ 'font': [] }],
+            [{ 'align': [] }],
+            ['link']         
+          ]
+        }
     }),
     SimplifiedUiModule,
   ],
